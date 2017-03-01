6ca92cd8b7bf8aeb0dc50a9f25449dbcb1129f52
elenameyerson <elena.meyerson@students.olin.edu>
2017-03-01 17:59:13 -0500

    Began implementing classes and functions.

--- snake.py
+++ snake.py
@@ -1,4 +1,14 @@
 '''
+pygame notes:
+-easy to define windows
+-easy to handle events (key presses, mouse motion)
+-drawing rectangles is well documented
+
+opencv:
+-easy to open camera
+-already has facial recognition code
+-can likely map that to tongue recognition
+
 classes:
 
 MVP
@@ -33,3 +43,46 @@ translate movements into game commands
 classes:
 scoreboard (displays score to player, possibly also high score)
 '''
+
+class Window:
+    """Represents the window that the game is played in.
+    Attributes include board length and width.
+    """
+
+    def __init__(self,length=1000,width=1000):
+        self.length = length
+        self.width = width
+        pass
+
+    #use pygame to implement showing window
+
+class Block:
+    """Represents the blocks that appear in the window and make up the food and
+    the parts of the snake.
+    """
+    def __init__(self, length = 10, width = 10, x, y, color):
+        self.length = length
+        self.width = width
+        self.x = x
+        self.y = y
+        self.color = color
+        pass
+
+    def showBlock(self):
+        pass
+
+    def moveBlock(self, dx, dy):
+        pass
+    #use pygame again lol
+
+class Food(Block):
+    """Represents the food that randomly appears in the window. Made up of randomly
+    placed blocks.
+    """
+    def __init__(self, color, point_value = 10):
+        self.color = color
+        self.point_value = point_value
+        pass
+
+    def random_placement(self):
+        
